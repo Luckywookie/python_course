5414f79cb09eb8338137c85f18c16baebfab9bd6
luckywookie <olga.belykh@gmail.com>
2016-10-21 00:43:47 +0300

    Сделана взаимосвязь между учеником и родителями

--- Course_python/lesson06/home_work/hw06_normal.py
+++ Course_python/lesson06/home_work/hw06_normal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
 
 # Задание-1:
 # Реализуйте описаную ниже задачу, используя парадигмы ООП:
@@ -8,39 +9,45 @@
 # свой определенный предмет. Т.е. Учитель Иванов может преподавать математику у 5А и 6Б, но больше математику не
 # может преподавать никто другой.
 
-
+# ***********************  Human  **********************************
 class Human(object):
     def __init__(self, name, surname, age):
         self.name = name
         self.age = age
         self.surname = surname
 
+    @property
     def view_fullname(self):
         return self.name + ' ' + self.surname
 
- #   def set_name(self, new_name):
- #       self.name = new_name
 
-
+# **************************  Parent  *******************************
 class Parent(object):
     def __init__(self, name_parent, surname_parent):
         self.name_parent = name_parent
         self.surname_parent = surname_parent
 
-
-class Student(Human, Parent):
-    def __init__(self, name, surname, age, class_number, name_parent, surname_parent):
-        Human.__init__(self, name, surname, age)
-        Parent.__init__(self, name_parent, surname_parent)
-        self.class_number = class_number
-
-    def has_parent(self):
+    @property
+    def fullname(self):
         return self.name_parent + ' ' + self.surname_parent
 
+
+# **************************  Student  *******************************
+class Student(Human):
+    def __init__(self, name, surname, age, class_number):
+        Human.__init__(self, name, surname, age)
+        self.class_number = class_number
+        self.parent = None
+
+    def has_parent(self, mother, father):
+        self.parent = mother + ' и ' + father
+        return 'Родители:  ' + self.parent
+
     def view_class_number(self):
-        return 'Ученик учится в классе: ', self.class_number
+        print 'Ученик', self.name, self.surname, 'учится в классе', self.class_number
 
 
+# **************************  Teacher  *******************************
 class Teacher(Human):
     def __init__(self, name, surname, age, lesson):
         Human.__init__(self, name, surname, age)
@@ -50,6 +57,15 @@ class Teacher(Human):
         return args
 
 
+parent1 = Human('Наталья', 'Новикова', 41)
+parent2 = Human('Олег', 'Новиков', 44)
+
+student1 = Student('Андрей', 'Новиков', 12, '5B')
+print student1.has_parent(parent1.view_fullname, parent2.view_fullname)
+student1.view_class_number()
+
+teacher1 = Teacher('Olga', 'Ivanova', 31, 'Math')
+print teacher1.teach_class('5B', '6B', '7D')
 
 
 # Выбранная и заполненная данными структура должна решать следующие задачи:
